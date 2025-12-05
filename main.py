3c7a10b51dbe124cc6728a0bea23a4ae0f04d451
nrg <reginzf@163.com>
2025-12-05 18:21:35 +0800

    新增自动标注的方法

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from pprint import pprint
 
+from label_studio_sdk.converter.imports.label_config import generate_label_config
+from openai import project
+from openpyxl.styles.builtins import title
+
 from src.core.login import LoginManager
 from src.core.api.knowledge_base.retriveve import Retrieve
 from src.core.format_func.zl_to_label_studio import retrieve_format_for_label_studio, doc_slices_format_for_label_studio
@@ -19,21 +23,42 @@ def main():
     # 获取指定doc下所有的切片
     from src.core.api.knowledge_base.knowledgeBase import KnowledgeBase
     knowledge_base_page = KnowledgeBase(login_manager)
-    doc_id = "DOC_d61cdd48e2c845aabcf436bce49eba8d"
-    # 获取doc所有的切片
+    kno_id = 'KLB_869cb0ded2c64362a2b5ce722d2e91cf'
+    doc_id = "DOC_cef4a72fac2045bc9c4c69de164be510"
+    # 获取doc所有的切片,转换为label_studio的格式
     doc_name, chunk_all = knowledge_base_page.doc_get_chunk_all(doc_id)
-    # 转换为label studio格式
-    # for ele in chunk_all:
-    #     pprint(ele)
     tasks = doc_slices_format_for_label_studio(doc_name, chunk_all)
+
+    # 登录label studio
     from model.label_studio.label_studio_client import label_studio_client
     from model.label_studio.task import create_tasks
-
-    project = label_studio_client.get_projects(title='切片项目1')[0]
-    create_tasks(project, tasks)
+    # 创建任务
+    # 根据问题配置文件创建标签
+    from model.label_studio.labels import LabelStudioXMLGenerator
+    from utils.pub_funs import load_json_file
+    question_json = load_json_file(r'D:\pyworkplace\git_place\ai-ken\tests\ospf\question.json')
+    # label_config = LabelStudioXMLGenerator().generate_from_json(question_json)
+    # project = label_studio_client.create_project(
+    #     title='ospf_chunk_test',
+    #     description="自动标注",
+    #     label_config=label_config,
+    #     show_instruction=True,
+    #     enable_empty_annotation=False,
+    # )
+    # create_tasks(project, tasks)
+    project = label_studio_client.get_projects(title='ospf_chunk_test')[0]
+    # 从紫鸾平台获取召回数据
+    retrieve = Retrieve(login_manager)
+    # 从question.json文件中获取问题和配置
+    from src.core.questions import get_questions
+    questions = get_questions(question_json, 'factual')
+    # 按问题进行召回，并进行标注
+    for question in questions['questions']:
+        recall_data = retrieve.webKnowledgeRetrieve('augmentedSearch', question, kno_id)
+        # 获取chunk_id，并执行标注
+        for chunk_id in recall_data['data']['chunk_ids']:
+            pass
 
 
 if __name__ == '__main__':
     main()
-
-
--- model/label_studio/annotator.py
+++ model/label_studio/annotator.py
@@ -1,5 +1,12 @@
+import jsonpath
 from label_studio_sdk._legacy import Project
 import requests
+
+VALUE_KEY_MAP = {
+    "Choice": 'choices'
+}
+
+FROM_NAME_SET = {'factual', 'contextual', 'conceptual', 'reasoning'}
 
 
 class Annotator:
@@ -8,7 +15,7 @@ class Annotator:
         self.url = project.url
         self.headers = project.headers
 
-    def generate_filter_item(self, value, filter, operator, type):
+    def generate_filter_item(self, value, filter, type="String", operator="equal"):
         """
         生成filter结构体
         :param value: 要过滤的值
@@ -38,23 +45,28 @@ class Annotator:
         for item in filter_items:
             filters["items"].append(self.generate_filter_item(**item))
         tasks = self.project.get_tasks(filters=filters)
-        self.project.get_task()
         return tasks
 
-    def create_annotation_data(self, value, from_name, to_name, type):
+    def create_annotation_data(self, value, from_name, to_name, type, result=None):
         # 构建标注结果
-        result = []
+        result = result if result is not None else []
         result.append({
             "from_name": from_name,
             "to_name": to_name,
             "type": type,
-            "value": value
+            "value": {
+                type: [value]}
         })
 
-    def create_annotation(self, task_id, annotation_data):
+    def create_annotation(self, task_id, annotation_data, http_method='POST'):
         """"""
         url = f"{self.url}/api/tasks/{task_id}/annotations?project={self.project.id}"
-        response = requests.post(url, headers=self.headers, json=annotation_data)
+        if http_method == 'POST':
+            response = requests.post(url, headers=self.headers, json=annotation_data)
+        elif http_method == 'PATCH':
+            response = requests.patch(url, headers=self.headers, json=annotation_data)
+        else:
+            raise ValueError("http_method参数错误,支持POST|PATCH")
         if response.status_code == 201:
             annotation_id = response.json().get('id')
             print(f"✅ 标注创建成功！标注ID: {annotation_id}")
@@ -64,10 +76,39 @@ class Annotator:
                 "data": response.json()
             }
         else:
-            print(f"❌ 标注创建失败: {response.status_code}")
-            print(f"   错误信息: {response.text}")
+            print(f"❌ 标注创建失败: {response.status_code},错误信息: {response.text}")
             return {
                 "success": False,
                 "error": response.text,
                 "status_code": response.status_code
             }
+
+    def task_annotate(self, task: dict, annotation_data):
+        """
+        对task进行标注
+        :param task_id:
+        :param annotation_data:
+        :return:
+        """
+        if task["total_annotations"] != 0:
+            # 获取所有的result
+            results = jsonpath.jsonpath(task, "$.annotations[*].result")[0]
+            results.append(annotation_data)
+            return self.create_annotation(task["id"], results, http_method='PATCH')
+        else:
+            return self.create_annotation(task["id"], annotation_data)
+
+
+if __name__ == '__main__':
+    # 在project下获得对应的task
+    from label_studio_sdk._legacy import Project
+    from model.label_studio.label_studio_client import label_studio_client
+
+    project = label_studio_client.get_projects(title="ospf_chunk_test")[0]
+    annotator = Annotator(project)
+    task_filter = annotator.generate_filter_item('5dd35eacd01f11f09ee902a65fdc6d6a', "filter:tasks:data.chunk_id")
+    target_task = annotator.get_task_by_filter([task_filter])
+    print(target_task)
+
+    # 获取对应chunk_id的task
+    # task = annotator.get_task_by_filter()
