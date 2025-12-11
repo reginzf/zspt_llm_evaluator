75cd3148b1f15db3b1ab03d8b4441016cfb995cb
nrg <reginzf@163.com>
2025-12-11 17:33:39 +0800

    新增多个chunk_size不同归档目录的功能

--- env_config_init.py
+++ env_config_init.py
@@ -1,5 +1,6 @@
 from dynaconf import Dynaconf
 from pathlib import Path
+
 settings = Dynaconf(
     envvar_prefix="ENV",
     settings_files=['configs/settings.toml'],
@@ -9,15 +10,18 @@ settings = Dynaconf(
 
 from utils.pub_funs import load_json_file, get_test_paths
 
-# 问题的JSON
+# 初始化特使用例目录
 paths = get_test_paths(settings.TEST_PATH)
-
+CHUNK_NAME = f'chunk_{settings.CHUNK_SIZE}_{settings.CHUNK_OVERLAP}'
 TARGET_DIR = paths["target_dir"]
 PROJECT_ROOT = paths["project_root"]
 QUESTION_PATH = paths["questions"]
 DOC_DIR = paths["docs_dir"]
-LS_LABELED_CHUNKS_DIR = paths["ls_labeled_chunks_dir"]
-ZLPT_CHUNKS_DIR = paths["lzpt_chunks_dir"]
+LS_LABELED_CHUNKS_DIR = str(Path(paths["ls_labeled_chunks_dir"]) / CHUNK_NAME)
+ZLPT_CHUNKS_DIR = str(Path(paths["lzpt_chunks_dir"]) / CHUNK_NAME)
+# 创建chunk归档目录
+Path(LS_LABELED_CHUNKS_DIR).mkdir(parents=True, exist_ok=True)
+Path(ZLPT_CHUNKS_DIR).mkdir(parents=True, exist_ok=True)
 
 QUESTION_TYPE_MAP = {
     "BASIC": 'basic_question.json',
@@ -27,4 +31,5 @@ QUESTION_TYPE_MAP = {
 }
 q_dir = Path(QUESTION_PATH)
 q_path = str(q_dir / QUESTION_TYPE_MAP[settings.QUESTION_TYPE])
+# 问题的JSON
 QUESTION_JSON = load_json_file(q_path)
