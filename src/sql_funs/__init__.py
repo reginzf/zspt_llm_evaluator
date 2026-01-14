b28399888a8661615fe645be27824b4dc46d86e1
nrg <reginzf@163.com>
2026-01-14 17:03:28 +0800

    sql增加导入

--- src/sql_funs/__init__.py
+++ src/sql_funs/__init__.py
@@ -5,3 +5,4 @@ from src.sql_funs.knowledge_crud import KnowledgeCrud
 from src.sql_funs.knowledge_path_crud import KnowledgePathCrud
 from src.sql_funs.questions_crud import QuestionsCRUD
 from src.sql_funs.label_studio_crud import LabelStudioCrud
+from src.sql_funs.metric_tasks_crud import MetricTasksCRUD
